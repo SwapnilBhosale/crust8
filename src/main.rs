18e96b85cc3638c11a4b65d800872fa35a52e851
SwapnilBhosale <sswapnil.1993@gmail.com>
2016-11-30 14:56:13 +0530

    started reading rom

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let rom_file_name = env::args().nth(1).unwrap();
+    let rom = read_bin(rom_file_name);
 }
+
+fn read_bin<P:AsRef<Path>>(path:P)-> Vec<u8> {
+    let mut file = File::open(path).unwrap();
+    let mut file_buf = Vec::new();
+    file.read_to_end(&mut file_buf).unwrap();
+    file_buf
+}
+
